11bbe9fcbc7a1235b9673703ac77fdb42f6d5d3a
tbrus <tomek.brus1@gmail.com>
2021-12-26 18:40:47 +0100

    v0.1.0

--- pyifc/__init__.py
+++ pyifc/__init__.py
@@ -4,8 +4,7 @@ https://github.com/tbrus/pyifc
 
 pyifc: Utilities for .ifc files
 
-pyifc is a Python package that provides utilities for working with 
-.ifc files.
+pyifc is a Python package that provides utilities for working with .ifc files.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.0.1"
