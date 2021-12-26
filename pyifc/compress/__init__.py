471013767454fa5be7e013ebb283b1e22cf5c906
tbrus <tomek.brus1@gmail.com>
2021-12-26 11:22:23 +0100

    Add pre-commit hooks (black, mypy, etc.)
    Add pre-commit hooks with tools and its config:
      - github/pre-commit-hooks
      - codespell
      - isort
      - black
      - flake8
      - mypy

--- pyifc/compress/__init__.py
+++ pyifc/compress/__init__.py
@@ -2,5 +2,9 @@
 pyifc.compress
 --------------
 
-Functions for compressing .ifc files.
+Compress and pack .ifc files.
 """
+from pyifc.compress._compress import compress
+from pyifc.compress._pack import compress_and_tar, compress_and_zip
+
+__all__ = ["compress", "compress_and_tar", "compress_and_zip"]
