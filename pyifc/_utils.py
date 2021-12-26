b179a84118979625cc171a11cdc0f9f1e28a9784
tbrus <tomek.brus1@gmail.com>
2021-12-26 12:53:12 +0100

    Add missing imports

--- pyifc/_utils.py
+++ pyifc/_utils.py
@@ -6,19 +6,25 @@ Utility functions.
 """
 
 import functools
+import time
 
 
 def timeit(func):
     """Decorator function for measuring function execution time.
-    
+
     The time is printed to stdout up to 4 decimal places.
     """
+
     @functools.wraps(func)
     def new_func(*args, **kwargs):
         start_time = time.time()
         result = func(*args, **kwargs)
         elapsed_time = time.time() - start_time
-        print('function [{}] finished in {} s'.format(
-            func.__name__, round(elapsed_time, 4)))
+        print(
+            "function [{}] finished in {} s".format(
+                func.__name__, round(elapsed_time, 4)
+            )
+        )
         return result
+
     return new_func
